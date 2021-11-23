020f7c0a6fb7c948653259c8a5e755f41b318e67
Dalrae1 <52721173+Dalrae1@users.noreply.github.com>
2021-11-22 17:54:57 -0800

    Attempt to get Python path
    This update should prevent issues with having multiple Python installations

--- Start Mumble Bot.bat
+++ Start Mumble Bot.bat
@@ -2,6 +2,25 @@
 TITLE Mumble Bot
 SET mypath=%~dp0
 SET PATH=%PATH%;%mypath%dependancies
-py "bot.py"
+
+goto :DOES_PYTHON_EXIST
+
+:DOES_PYTHON_EXIST
+py --version 2>NUL
+if errorlevel 1 goto :PYTHON_DOES_NOT_EXIST
+goto :PYTHON_DOES_EXIST
+
+:PYTHON_DOES_NOT_EXIST
+echo Python is not installed. Install Python 3.10
+start "" "https://www.python.org/downloads/windows/"
+goto :EOF
+
+:PYTHON_DOES_EXIST
+for /f "delims=" %%V in ('where py') do @set pythonpath=%%V
+for /f "delims=" %%V in ('%pythonpath% --version') do @set pythonversion=%%V 
+echo Using %pythonversion% at %pythonpath%
+
+%pythonpath% "bot.py"
+
 echo Process Terminated.
 pause
